c2f8e8f137922f87c6eae6e2e1bfa490b597cd34
Max Techera <maxi.techerag@gmail.com>
2018-09-04 23:45:01 -0300

    Add inmemory database with entity framework

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -11,13 +11,24 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodosController : ControllerBase
     {
+        private readonly TodoContext _context;
+
+        public TodosController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
         [HttpGet]
         public ActionResult<List<TodoItem>> Get() {
-            return new List<TodoItem>() {
-               new TodoItem { Name = "Item1" },
-               new TodoItem { Name = "Item2" },
-               new TodoItem { Name = "Item3" },
-            };
+            return _context.TodoItems.ToList();
         }
     }
 }
